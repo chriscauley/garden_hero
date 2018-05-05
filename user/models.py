9b1720a097a41d0ef508df2eb5472c9d6da659b9
chriscauley <chris@lablackey.com>
2018-05-05 18:19:13 -0400

    adding phone number field to user

--- user/models.py
+++ user/models.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 class User(AbstractUser):
-  pass
+  _m = "Phone number must be entered in the format: '999999999'. Up to 15 digits allowed."
+  phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=_m)
+  phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
