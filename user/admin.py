dcd0cc81ec6d4c65ca703196639d498266d02ed8
chriscauley <chris@lablackey.com>
2018-05-05 18:19:23 -0400

    adding user admin

--- user/admin.py
+++ user/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from models import User
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+  fieldsets = list(UserAdmin.fieldsets)
+  fieldsets[1] = list(fieldsets[1])
+  fieldsets[1][1]['fields'] = ('first_name', 'last_name', 'email', 'phone_number')
+
+
