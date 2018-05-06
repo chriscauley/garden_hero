8606d21e0c263356f04657aa491474a4174e43c9
chriscauley <chris@lablackey.com>
2018-05-05 23:59:45 -0400

    adding urrent_plots

--- user/admin.py
+++ user/admin.py
@@ -11,5 +11,4 @@ class UserAdmin(UserAdmin):
   fieldsets = list(UserAdmin.fieldsets)
   fieldsets[1] = list(fieldsets[1])
   fieldsets[1][1]['fields'] = ('first_name', 'last_name', 'email', 'phone_number')
-
-
+  list_display = ('email','get_full_name','current_plots')
--- user/models.py
+++ user/models.py
@@ -5,8 +5,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
+import datetime
+
 class User(AbstractUser):
   _m = "Phone number must be entered in the format: '999999999'. Up to 15 digits allowed."
   phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=_m)
   phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-
+  def current_plots(self):
+    today = datetime.date.today()
+    ums = self.subscription_set.filter(start_date__gte=today,canceled__isnull=True)
+    return [u.plot for u in ums]
