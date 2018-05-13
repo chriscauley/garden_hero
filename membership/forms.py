590c78dafa1f0fdb97c6bc49b8d2474a4ef0a3b5
chriscauley <chris@lablackey.com>
2018-05-13 10:58:04 -0400

    adding endpoints to site map and fixing bug on profile/registration forms

--- membership/forms.py
+++ membership/forms.py
@@ -1,16 +1,33 @@
 from django import forms
 
-from lablackey.forms import RequestModelForm
+from lablackey.forms import RequestModelForm,RequestForm
 
 from user.models import User, UserInvite
 
+class SigninForm(RequestForm):
+  username = forms.CharField(max_length=64)
+  password = forms.CharField(max_length=64)
+  @classmethod
+  def user_is_allowed(cls,request):
+    return True
+  class Meta:
+    widgets = { 'password': forms.PasswordInput(), }
+
 class ProfileForm(RequestModelForm):
+  @classmethod
+  def user_is_allowed(cls,request):
+    #! TODO!!
+    return True
   class Meta:
     model = User
     fields = ("email","first_name","last_name","phone_number","address1","address2","city","state",)
 
 class RegistrationForm(RequestModelForm):
   uuid = forms.CharField(required=False,widget=forms.HiddenInput(),max_length=32)
+  @classmethod
+  def user_is_allowed(cls,request):
+    #! TODO!!
+    return True
   def __init__(self,request,*args,**kwargs):
     kwargs['initial'] = kwargs.get("initial",None) or {}
     state = "PA"
